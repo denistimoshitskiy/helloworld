f608187a5df81033cf52c1044da3a960283b147c
Denis Timoshitskiy <45280161+denistimoshitskiy@users.noreply.github.com>
2025-09-15 15:51:29 +0200

    Попытка с IF сделать

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,55 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Привет мир!");
+import  java.util.Scanner;
+import java.util.Random;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    private static final Random random = new Random(); // Создание объекта Random для генерации случайного числа для выбора слова в игре
+    public static String createSecretWord(String[] selectedWordArray) { // Создание маски слова, количество звездочек равно количеству букв в загаданном слове
+        return "*".repeat(selectedWordArray.length);
+    }
+    public static void main(String[] args) {
+        String [] listWords = {"лошадь", "напиток"}; // Список слов
+        Scanner scanner = new Scanner(System.in); // Создаем объект scanner для возможности считывания ответа из консоли
+        System.out.println("Вы хотите начать новую игру? Y/N"); // Спрашиваем пользователя о желании начать новую игру
+        String answer = scanner.nextLine(); // Запускаем считываем того, что пользователь ввел из консоли
+        int randomIndex = random.nextInt(listWords.length); // Генерация случайного числа
+        String selectedWord = listWords[randomIndex]; // Выбор случайного слова из массива
+        String selectedWordArray[] = selectedWord.split(""); // Разделение строки загаданного слова на элементы массива
+        if (answer.equals("y")) { // Если пользователь ответил Y - игра стартует
+            System.out.println("Игра начинается");
+            System.out.println("Я загадал слово " + createSecretWord(selectedWordArray));
+            System.out.println("Введи букву, которая может быть в слове");
+            String firstLetter = scanner.nextLine();
+            if (selectedWord.contains(firstLetter)) { // Первая попытка
+                for (int i = 0; i < selectedWordArray.length; i++) {
+                    if (selectedWordArray[i].equals(firstLetter)){
+                        selectedWordArray[i] = firstLetter;
+                    } else {
+                        selectedWordArray[i] = "*";
+                    }
+                }
+                String selectedWordAfterFirstChance = String.join("", selectedWordArray);
+                String selectedWordAfterFirstChanceArray[] = selectedWordAfterFirstChance.split("");
+                System.out.println("Такая буква есть! Открыта часть слова " + selectedWordAfterFirstChance);
+                System.out.println("Введи следующую букву букву, которая может быть в слове");
+                String secondLetter = scanner.nextLine();
+                if (selectedWord.contains(secondLetter)) { // Вторая попытка
+                    for (int i = 0; i < selectedWordAfterFirstChanceArray.length; i++) {
+                        if (selectedWordAfterFirstChanceArray[i].equals(secondLetter)){
+                            selectedWordAfterFirstChanceArray[i] = secondLetter;
+                        } else {
+                            selectedWordAfterFirstChanceArray[i] = "*";
+                        }
+                    }
+                    String secondChance = String.join("", selectedWordAfterFirstChanceArray);
+                    System.out.println("Такая буква есть! Открыта часть слова " + secondChance);
+                } else {
+                    System.out.println("Такой буквы нет! Повесим голову и туловище :)");
+                }
+            } else {
+                System.out.println("Такой буквы нет! Повесим голову :)");
+            }
+        } else if (answer.equals("n")) {
+            System.out.println("Вы вышли");
         }
     }
 }
