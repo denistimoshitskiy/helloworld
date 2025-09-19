13cfdf2bb4da23382fde0e9a18b6a17d4d65af8e
Denis Timoshitskiy <45280161+denistimoshitskiy@users.noreply.github.com>
2025-09-19 16:14:48 +0200

    With while

--- src/Main.java
+++ src/Main.java
@@ -1,52 +1,60 @@
 import  java.util.Scanner;
 import java.util.Random;
+import java.util.Arrays;
 
 public class Main {
     private static final Random random = new Random(); // Создание объекта Random для генерации случайного числа для выбора слова в игре
-    public static String createSecretWord(String[] selectedWordArray) { // Создание маски слова, количество звездочек равно количеству букв в загаданном слове
-        return "*".repeat(selectedWordArray.length);
+    public static String createSecretWord(String word) {
+        return "*".repeat(word.length());
     }
     public static void main(String[] args) {
         String [] listWords = {"лошадь", "напиток"}; // Список слов
+        int randomIndex = random.nextInt(listWords.length); // Генерация случайного числа
+        String selectedWord = listWords[randomIndex]; // Выбор случайного слова из массива
+        String[] selectedWordArray = new String[selectedWord.length()];
+
+        // Инициализируем массив звездочками
+        for (int i = 0; i < selectedWordArray.length; i++) {
+            selectedWordArray[i] = "*";
+        }
+
         Scanner scanner = new Scanner(System.in); // Создаем объект scanner для возможности считывания ответа из консоли
         System.out.println("Вы хотите начать новую игру? Y/N"); // Спрашиваем пользователя о желании начать новую игру
         String answer = scanner.nextLine(); // Запускаем считываем того, что пользователь ввел из консоли
-        int randomIndex = random.nextInt(listWords.length); // Генерация случайного числа
-        String selectedWord = listWords[randomIndex]; // Выбор случайного слова из массива
-        String selectedWordArray[] = selectedWord.split(""); // Разделение строки загаданного слова на элементы массива
-        if (answer.equals("y")) { // Если пользователь ответил Y - игра стартует
-            System.out.println("Игра начинается");
-            System.out.println("Я загадал слово " + createSecretWord(selectedWordArray));
-            System.out.println("Введи букву, которая может быть в слове");
-            String firstLetter = scanner.nextLine();
-            if (selectedWord.contains(firstLetter)) { // Первая попытка
-                for (int i = 0; i < selectedWordArray.length; i++) {
-                    if (selectedWordArray[i].equals(firstLetter)){
-                        selectedWordArray[i] = firstLetter;
-                    } else {
-                        selectedWordArray[i] = "*";
-                    }
+        String answerWord = "";
+        int attempt = 0;
+        int errors = 0;
+        if (answer.equals("y")) { // Если пользователь ввел y, то игра начинается
+            while(selectedWord != answerWord) {
+                attempt++;
+                if(attempt == 1){
+                    System.out.println("Я загадал слово " + createSecretWord(selectedWord));
                 }
-                String selectedWordAfterFirstChance = String.join("", selectedWordArray);
-                String selectedWordAfterFirstChanceArray[] = selectedWordAfterFirstChance.split("");
-                System.out.println("Такая буква есть! Открыта часть слова " + selectedWordAfterFirstChance);
-                System.out.println("Введи следующую букву букву, которая может быть в слове");
-                String secondLetter = scanner.nextLine();
-                if (selectedWord.contains(secondLetter)) { // Вторая попытка
-                    for (int i = 0; i < selectedWordAfterFirstChanceArray.length; i++) {
-                        if (selectedWordAfterFirstChanceArray[i].equals(secondLetter)){
-                            selectedWordAfterFirstChanceArray[i] = secondLetter;
-                        } else {
-                            selectedWordAfterFirstChanceArray[i] = "*";
+                if (errors < 5) {
+                    System.out.println("Введи букву, которая может быть в слове");
+                    String letter = scanner.nextLine();
+                    if (selectedWord.contains(letter)) {
+                        for (int i = 0; i < selectedWordArray.length; i++) {
+                            if (selectedWordArray[i].equals(letter)) {
+                                selectedWordArray[i] = letter;
+                            }
                         }
+                        String newStatusSelectedWord = String.join("", selectedWordArray);
+                        System.out.println("Пока угадано " + newStatusSelectedWord);
+                        if (newStatusSelectedWord.equals(selectedWord)) {
+                            System.out.println("Поздравляю слово отгадано!");
+                            break;
+                        }
+                    } else {
+                        errors++;
+                        System.out.println("Ошибок " + errors);
                     }
-                    String secondChance = String.join("", selectedWordAfterFirstChanceArray);
-                    System.out.println("Такая буква есть! Открыта часть слова " + secondChance);
-                } else {
-                    System.out.println("Такой буквы нет! Повесим голову и туловище :)");
                 }
-            } else {
-                System.out.println("Такой буквы нет! Повесим голову :)");
+                else {
+                    System.out.println("Вы проиграли");
+                    break;
+                }
+
             }
         } else if (answer.equals("n")) {
             System.out.println("Вы вышли");
---
+++ src/Main2.java
@@ -0,0 +1,55 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main2 {
+    private static final Random random = new Random(); // Создание объекта Random для генерации случайного числа для выбора слова в игре
+    public static String createSecretWord(String[] selectedWordArray) { // Создание маски слова, количество звездочек равно количеству букв в загаданном слове
+        return "*".repeat(selectedWordArray.length);
+    }
+    public static void main(String[] args) {
+        String [] listWords = {"лошадь", "напиток"}; // Список слов
+        Scanner scanner = new Scanner(System.in); // Создаем объект scanner для возможности считывания ответа из консоли
+        System.out.println("Вы хотите начать новую игру? Y/N"); // Спрашиваем пользователя о желании начать новую игру
+        String answer = scanner.nextLine(); // Запускаем считываем того, что пользователь ввел из консоли
+        int randomIndex = random.nextInt(listWords.length); // Генерация случайного числа
+        String selectedWord = listWords[randomIndex]; // Выбор случайного слова из массива
+        String selectedWordArray[] = selectedWord.split(""); // Разделение строки загаданного слова на элементы массива
+        if (answer.equals("y")) { // Если пользователь ответил Y - игра стартует
+            System.out.println("Игра начинается");
+            System.out.println("Я загадал слово " + createSecretWord(selectedWordArray));
+            System.out.println("Введи букву, которая может быть в слове");
+            String firstLetter = scanner.nextLine();
+            if (selectedWord.contains(firstLetter)) { // Первая попытка
+                for (int i = 0; i < selectedWordArray.length; i++) {
+                    if (selectedWordArray[i].equals(firstLetter)){
+                        selectedWordArray[i] = firstLetter;
+                    } else {
+                        selectedWordArray[i] = "*";
+                    }
+                }
+                String selectedWordAfterFirstChance = String.join("", selectedWordArray);
+                String selectedWordAfterFirstChanceArray[] = selectedWordAfterFirstChance.split("");
+                System.out.println("Такая буква есть! Открыта часть слова " + selectedWordAfterFirstChance);
+                System.out.println("Введи следующую букву букву, которая может быть в слове");
+                String secondLetter = scanner.nextLine();
+                if (selectedWord.contains(secondLetter)) { // Вторая попытка
+                    for (int i = 0; i < selectedWordAfterFirstChanceArray.length; i++) {
+                        if (selectedWordAfterFirstChanceArray[i].equals(secondLetter)){
+                            selectedWordAfterFirstChanceArray[i] = secondLetter;
+                        } else {
+                            selectedWordAfterFirstChanceArray[i] = "*";
+                        }
+                    }
+                    String secondChance = String.join("", selectedWordAfterFirstChanceArray);
+                    System.out.println("Такая буква есть! Открыта часть слова " + secondChance);
+                } else {
+                    System.out.println("Такой буквы нет! Повесим голову и туловище :)");
+                }
+            } else {
+                System.out.println("Такой буквы нет! Повесим голову :)");
+            }
+        } else if (answer.equals("n")) {
+            System.out.println("Вы вышли");
+        }
+    }
+}
